ff1c15404ea855c2077752b53fae9ba86b29f8db
jwdeitch <jwdeitch@gmail.com>
2018-01-04 21:40:58 -0500

    init

---
+++ src/extension.c
@@ -0,0 +1,34 @@
+#include "postgres.h"
+#include "fmgr.h"
+#include "utils/builtins.h"
+#include "utils/guc.h"
+
+PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(base36_encode);
+
+
+Datum
+base36_encode(PG_FUNCTION_ARGS) {
+	int32 arg = PG_GETARG_INT32(0);
+	if (arg < 0)
+		ereport(ERROR,
+				(
+						errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+								errmsg("negative values are not allowed"),
+								errdetail("value %d is negative", arg),
+								errhint("make it positive")
+				)
+		);
+
+	char *message_txt = GetConfigOptionByName("demopgextension.message_txt", NULL,
+											  true);
+
+	if (message_txt == NULL)
+		message_txt = "hello";
+
+	// You've entered
+	char *result = sprintf("%s: %i", message_txt, arg);
+
+	PG_RETURN_TEXT_P(cstring_to_text(&result));
+}
